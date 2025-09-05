2af15e996d3e59a2e38d928c7a5002f3d4508e79
olejpet1 <olejpeti@gmail.com>
2025-09-05 12:15:54 +0200

    done

--- pro.py
+++ pro.py
@@ -1,1 +1,26 @@
-print("Szia")
+print("Szia" , end=" ")
+print("Tibi")
+print("Szia" , "Tóth " , "Karcsi" , "!" , sep="")
+print("Szia Tóth Karcsi!")
+nev = "Tóth Karcsi"
+print(f"Szia {nev} !")
+print("Szia","Peti", sep="\n" , end="!")
+#adattipusok 
+# int()
+# float()
+# str()
+# bool()
+# list()
+# set()
+print()
+print(int("5"))
+print(float(5))
+print(type(str(5.0)))
+print(bool(-1))
+print(["hétfo" , "kedd" , "szerda"])
+napok = ["hétfo" , "kedd" , "szerda"]
+print(f"Napok: {napok}")
+nevek = ["Tibi" , "Sanyi" , "Tibi"]
+print(f"Nevek: {set(nevek)}")
+print(tuple([1,2,3]))
+
