757f14ac786bd6580b7d5e9b3a9808801a101fef
olejpet1 <131852060+olejpet1@users.noreply.github.com>
2025-09-11 22:31:44 +0200

    nemtom  start.py

--- start.py
+++ start.py
@@ -1,26 +1,32 @@
-#feladat: negyzet/teglalap keuletenek szamitasa
+# feladat: négyzet/téglalap kerületének számítása
 def beker(alakzat, oldal):
-	"""""Bekéri az 'alakzat' 'oldal' oldalanak hosszat"""
-	oldalhossz = int(input("Kérem a {alakzat} {oldal} oldalanak hosszat [cm]:"))
-	return oldalhossz
-#def	teglalapKerulete(a, b)
-#	"""kiszamolja az 'a' es 'b' oldal ismereteben a teglalap keruletet K=2*(a+b)"""
-	kerulet = 2* (a+b)
-def kiir(alakzat, kerulet):
+    """Bekéri az 'alakzat' 'oldal' oldalának hosszát"""
+    oldalhossz = int(input(f"Kérem a {alakzat} {oldal} oldalának hosszát [cm]: "))
+    return oldalhossz
 
-#input  m = input("[T]églalap vagy [N]égyzet keruletet szamoljam? [T|N]")
-#if mit.upper() == "N":
-	alap = beker("négyzet", "a")
-	#calculation
-	kerulet = teglalapKerulete(alap, alap)
-	#output 
-	print(f"A negyzet kerulete: {kerulet}cm. ")
-#elif mit.upper() == "T" :
-	a_oldal = beker("téglalap" , "a")
-	b_oldal = beker("téglalap" , "b")
-	#calculation
-	kerulet = teglalapKerulete(a_oldal, b_oldal )
-	#output 
-	print(f"A negyzet kerulete: {kerulet}cm. ")
-#else:
-	print("Hibá választás. Kilépek")
+def teglalapKerulete(a, b):
+    """ kiszámolja az 'a' és 'b' oldal ismeretében a téglalap kerületét. K= 2*(a+b)"""
+    kerulet = 2 * (a + b)
+    return kerulet
+
+def kiir(alakzat, kerulet):
+    """Kiírja az 'alakzat' 'kerulet'-ét """
+    print(f"A {alakzat} kerülete: {kerulet}cm.")
+
+# input
+mit = input("[T]églalap vagy [N]égyzet kerületét számoljam [T|N]?") 
+if mit.upper() == "N":
+    alap = beker("négyzet", "a")
+    # calculation
+    kerulet = teglalapKerulete(alap, alap)
+    # output
+    kiir("négyzet", kerulet)
+elif mit.upper() == "T":
+    a_oldal = beker("téglalap","a")
+    b_oldal = beker("téglalap","b")
+    # calculation
+    kerulet = teglalapKerulete(a_oldal, b_oldal)
+    # output
+    kiir("teglalap", kerulet)
+else:
+    print("Hibás választás. Kilépek!")
