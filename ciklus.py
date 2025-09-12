f96786d6ec62bb1e96b554787df65ef733d96191
olejpet1 <olejpeti@gmail.com>
2025-09-12 12:35:49 +0200

    szorzotabla

--- ciklus.py
+++ ciklus.py
@@ -15,3 +15,30 @@ def paros(lista):
 
 
 print(f"A lista paris elemeinek osszege {paros(lista)}")
+
+#irass ki egy sorba 5 * ot
+print("*"*5)
+
+for i in range(5):
+	print("*", end="")
+print("")
+
+#rajzolj piramist 
+print("    *")
+print("   ***")
+print("  *****")
+print(" ********")
+
+n = 9
+space_db= n - 1 
+csillag_db = 1
+for i in range(n):
+	print(" " * space_db, end="")
+	print("*" * csillag_db)
+	space_db -=1
+	csillag_db +=2
+
+for i in range (1, 11):
+	for j in range (1, 11):
+		print(f"{i*j:10}", end=" ")
+	print()
