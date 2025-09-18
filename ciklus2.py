1278c739c877dc9c15618995f0262bfc2bc61ab1
olejpet1 <olejpeti@gmail.com>
2025-09-18 18:53:49 +0200

    hazif

--- ciklus2.py
+++ ciklus2.py
@@ -1,1 +1,22 @@
-while bennmaradasi feltetel:
+# Kérjünk be számokat és adjuk meg az összegüket
+# 1 0 végjelig
+# 2 Enter végjelig HF
+osszeg=0
+be_n=1
+while be_n !=0:
+    be_n=int(input("kérek egy számot: "))
+    osszeg+=be_n
+
+print(osszeg)
+
+
+osszeg = 0
+be = "0"   # nem üres, hogy belépjen a ciklusba
+
+while be != "":
+    be = input("Kérek egy számot (Enter a vége): ")
+    if be != "":          # csak akkor adja hozzá, ha tényleg számot írtunk be
+        osszeg += int(be)
+
+print("Az összeg:", osszeg)
+
