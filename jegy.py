8bf4508646fa7e39650b6de7703e08ab335b919a
olejpet1 <olejpeti@gmail.com>
2025-09-26 12:08:46 +0200

    orainevek

---
+++ jegy.py
@@ -0,0 +1,16 @@
+spec_betuk = ["Dzs", "Cs", "Dz", "Gy", "Ly", "Ny", "Sz", "Ty", "Zs"]
+
+nev = input("Add meg a teljes neved: ")
+
+szavak = nev.strip().split()
+
+monogram = ""
+
+for szo in szavak:
+    if len(szo) >= 3 and (szo[0].upper() + szo[1].lower() + szo[2].lower()) in spec_betuk:
+        monogram += szo[0].upper() + szo[1].lower() + szo[2].lower()
+    elif len(szo) >= 2 and (szo[0].upper() + szo[1].lower()) in spec_betuk:
+        monogram += szo[0].upper() + szo[1].lower()
+    else:
+        monogram += szo[0].upper()
+print("A monogramod:", monogram)
--- szamolo.py
+++ szamolo.py
@@ -1,35 +1,42 @@
-lista=[1,1,2,2,4,5,2,1,3]
-egyes_db=0
-kettes_db=0
-harmas_db=0
-negyes_db=0
-otos_db=0
+szamok=[1,1,2,2,4,5,2,1,3]
+# egyes_db=0
+# kettes_db=0
+# harmas_db=0
+# negyes_db=0
+# otos_db=0
 
-for szam in lista:
-	if szam==1:
-		egyes_db+=1
-	elif szam==2:
-		kettes_db+=1
-	elif szam==3:
-		harmas_db+=1
-	elif szam==4:
-		negyes_db+=1
-	elif szam==5:
-		otos_db+=1
-print(f"Az 1-es szambol ennyi db van {egyes_db}")
-print(f"Az 1-es szambol ennyi db van {kettes_db}")
-print(f"Az 1-es szambol ennyi db van {harmas_db}")
-print(f"Az 1-es szambol ennyi db van {negyes_db}")
-print(f"Az 1-es szambol ennyi db van {otos_db}")
-
-
-db=lista.count(1)
-db2=lista.count(2)
-db3=lista.count(3)
-db4=lista.count(4)
-db5=lista.count(5)
-print(db,db2,db3,db4,db5)
+# for szam in szamok:
+# 	if szam==1:
+# 		egyes_db+=1
+# 	elif szam==2:
+# 		kettes_db+=1
+# 	elif szam==3:
+# 		harmas_db+=1
+# 	elif szam==4:
+# 		negyes_db+=1
+# 	elif szam==5:
+# 		otos_db+=1
+# print(f"Az 1-es szambol ennyi db van {egyes_db}")
+# print(f"Az 1-es szambol ennyi db van {kettes_db}")
+# print(f"Az 1-es szambol ennyi db van {harmas_db}")
+# print(f"Az 1-es szambol ennyi db van {negyes_db}")
+# print(f"Az 1-es szambol ennyi db van {otos_db}")
 
 
-for i in range(1,6):
-	print(f"{i} bol van {lista.count(i)}")
+# db=szamok.count(1)
+# db2=szamok.count(2)
+# db3=szamok.count(3)
+# db4=szamok.count(4)
+# db5=szamok.count(5)
+# print(db,db2,db3,db4,db5)
+
+
+# for i in range(1,6):
+# 	print(f"{i} bol van {szamok.count(i)}")
+darab=[0,0,0,0,0]
+for szam in szamok:
+	darab[szam-1]+=1
+print(darab)
+
+for i in range(len(darab)):
+	print(f"{i+1}-bol van {darab[i]}")
